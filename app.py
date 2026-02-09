29691cbc9e887ee11f30fdd0cbe106c93b5e4364
KadoliShweta29 <kadolishweta29@gmail.com>
2026-02-09 22:03:22 +0530

    content changed

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Docker CI/CD App Running!"
+    return "This is CICD using all CICD tools and AWS"
 
 app.run(host="0.0.0.0", port=5000)
