0f413e7f5e83e02461daa11703382adbbf22113c
KadoliShweta29 <kadolishweta29@gmail.com>
2026-02-25 23:20:35 +0530

    Update home route response message

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "This is CICD using all CICD tools and AWS @@@@"
+    return "This is CICD using all CICD tools and AWS @@@@sxdcvbnmsdfghjkxcvbn"
 
 app.run(host="0.0.0.0", port=5000)
